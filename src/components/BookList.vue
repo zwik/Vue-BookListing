f46c251732ce2b4c42ac9488bbe0c831fab90133
Marcel <m.v.d.bosch@gmail.com>
2018-11-19 15:26:15 +0100

    Added BookForm integration

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -4,16 +4,20 @@
         <ul>
             <book-item v-for="book in books" :book='book'></book-item>
         </ul>
+        <hr>
+        <book-form @addBook='appendBook'></book-form>
     </div>
 </template>
 
 <script>
 import BookItem from './BookItem';
+import BookForm from './BookForm';
 
 export default {
     name: 'BookList',
     components: {
-        BookItem
+        BookItem,
+        BookForm
     },
     data() {
         return {
@@ -24,6 +28,11 @@ export default {
                 {title: 'Amusing Ourselves to Death', author: 'Neil Postman'}
             ]
         };
+    },
+    methods: {
+        appendBook(bookTitle, bookAuthor) {
+            this.books.push({title: bookTitle, author: bookAuthor});
+        }
     }
 }
 </script>
